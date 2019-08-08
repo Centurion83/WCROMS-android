19873ee9a44426cc81c4e22a99c3a8d47014b8f7
Josh Chasky <jchasky@gmail.com>
2019-08-08 19:03:10 -0400

    Damn, ni**as wanna stick me for my paper
    Change-Id: I3f50dd4b7340a2c7c49dddc399f02b6e7d22f1b4

---
+++ config/branding.mk
@@ -0,0 +1,30 @@
+# Versioning System
+DU_BASE_VERSION = v13.5
+
+ifndef DU_BUILD_TYPE
+    DU_BUILD_TYPE := UNOFFICIAL
+endif
+
+# Only include DU-Updater for official, weeklies, and rc builds
+ifeq ($(filter-out OFFICIAL WEEKLIES RC,$(DU_BUILD_TYPE)),)
+    PRODUCT_PACKAGES += \
+        DU-Updater
+endif
+
+# Sign builds if building an official or weekly build
+ifeq ($(filter-out OFFICIAL WEEKLIES,$(DU_BUILD_TYPE)),)
+    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(KEYS_LOCATION)
+endif
+
+# Set all versions
+DATE := $(shell date -u +%Y%m%d)
+TIME := $(shell date -u +%H%M)
+DU_VERSION := $(TARGET_PRODUCT)-$(DU_BASE_VERSION)-$(DATE)-$(TIME)-$(DU_BUILD_TYPE)
+TARGET_BACON_NAME := $(DU_VERSION)
+ROM_FINGERPRINT := DirtyUnicorns/$(PLATFORM_VERSION)/$(DU_BUILD_TYPE)/$(DATE)$(TIME)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    BUILD_DISPLAY_ID=$(BUILD_ID) \
+    ro.du.version=$(DU_VERSION) \
+    ro.mod.version=$(DU_BUILD_TYPE)-$(DU_BASE_VERSION)-$(DATE) \
+    ro.du.fingerprint=$(ROM_FINGERPRINT)
